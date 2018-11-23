59d80215179b0fb1e95d99885e5ddb8c3b7c0497
Gautam <gramasu000@citymail.cuny.edu>
2018-11-23 16:10:58 -0500

    Added comments to app initialization and database creation

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,26 +2,47 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
+    """
+    Application Factory Function
+    Creates the Flask instance, configures it and returns it
+    This is better than having the Flask instance as a global variable.
+    """
+
+    # When creating the flask instance,
+    #   we pass in the module name __name__
+    #   and we tell Flask to search in instance/ directory
+    #   for database and config files private to the website. 
     app = Flask(__name__, instance_relative_config=True)
+
+    # Sets up default configuration
+    #   SECRET_KEY will be a key to encrypt data, set to "dev" during development
+    #   DATABASE will be the path where the database instance will be stored
+    #       it is set to be inside Flask instance/ directory.
     app.config.from_mapping(
         SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
     
+    # Loads normal configuration file in instance/ folder,
+    #   or test configuration file if required.  
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
 
+    # Ensure that the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
+    # When the web url is <url>/hello, 
+    #   we load a simple hello world page
     @app.route('/hello')
     def hello():
         return "<h1>Hello World!</h1>"
 
+    # Register init_db_command and close_db from db.py file
     from . import db
     db.init_app(app)
 
--- flaskr/db.py
+++ flaskr/db.py
@@ -4,33 +4,61 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 def get_db():
+    """
+    Creates a connection to the database instance
+        in the instance/ directory.
+    Returns the database object
+    Also attaches it to global object g
+    """
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES,
         )
+        
+        # Rows in sqlite will be dicts in python
         g.db.row_factory = sqlite3.Row
 
     return g.db
 
 def close_db(e=None):
+    """
+    Checks if database object exists in g
+        if it does, it is popped from g and closed
+    """
     db = g.pop('db', None)
     
     if db is not None:
         db.close()
 
 def init_db():
+    """
+    Create a db object using get_db
+        and run sql code in schema.sql to create tables
+    """
     db = get_db();
     
+    # Use current_app.open_resource to open a file relative to flask app
+    #   Use read() decode() and executescript to 
+    #       to interpret text as sql instructions 
     with current_app.open_resource("schema.sql") as f:
         db.executescript(f.read().decode("utf-8"))
 
 @click.command("init-db")
 @with_appcontext
 def init_db_command():
+    """
+    This function initializes the db using init_db
+        and prints a message to console.
+    The function is linked to a newly created flask command init-db
+        by the @click.command/@with_appcontext decorators
+    """
     init_db()
     click.echo("Initialized the database")
 
 def init_app(app):
+    """
+    We register the close_db and init_db_command with the application 
+    """
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
